83d59df2c415e4e02a08365a2f1edc6224a58037
Pranav-Talmale <67180305+Pranav-Talmale@users.noreply.github.com>
2022-06-27 22:26:52 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export TWRP_BRANCH="twrp-12.1"
-export DT_LINK="https://github.com/Pranav-Talmale/android_device_xiaomi_raphael-twrp.git -b twrp-12.1"
+export DT_LINK="https://github.com/Pranav-Talmale/android_device_xiaomi_raphael-twrp.git -b normal-fstab"
 
 export DEVICE="raphael"
 export OEM="xiaomi"
